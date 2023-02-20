e97b46aa8fcc46fcf2e681a8986e98474604a51c
bhuvanesh14 <bhuvanesh14@users.noreply.github.com>
2023-02-21 00:23:15 +0530

    basic degugger js update

---
+++ assignment.js
@@ -0,0 +1,11 @@
+
+//let fec = document.getElementById('userinput');
+let currentnum = 20;
+let currentres;
+
+currentres = 18 + currentnum;
+currentres = currentres - 10;
+currentres = currentres * 2;
+currentres = currentres / 10;
+console.log(currentres);
+//document.getElementById('result2');
